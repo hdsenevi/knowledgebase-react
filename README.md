f376901ea5290b14732ffb40173e191685f6c425
Shanaka Senevirathne <shanaka.gunasekarasenevirathne@nab.com.au>
2020-12-18 18:41:08 +1100

    few changes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Knowledge base - Career Development
+# Knowledge base - react
 
-https://hdsenevi.github.io/knowledgebase-career-development/
+https://hdsenevi.github.io/knowledgebase-react/
 
 [gatsby-gitbook-starter readme](docs/gatsby-gitbook-starter-readme.md)
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   gatsby: {
-    pathPrefix: '/knowledgebase-career-development',
+    pathPrefix: '/knowledgebase-react',
     siteUrl: 'https://hdsenevi.github.io',
     gaTrackingId: null,
     trailingSlash: false,
@@ -10,7 +10,7 @@ const config = {
     logoLink: 'https://hasura.io/learn/',
     title:
       "<a href='https://hasura.io/learn/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
-    githubUrl: 'https://hdsenevi.github.io/knowledgebase-career-development',
+    githubUrl: 'https://hdsenevi.github.io/knowledgebase-react',
     helpUrl: '',
     tweetText: '',
     social: `<li>
@@ -54,8 +54,8 @@ const config = {
     title: 'Knowledge base career development',
     description: 'My personal knowledge base for career development',
     ogImage: null,
-    docsLocation: 'https://github.com/hdsenevi/knowledgebase-career-development/tree/master/content',
-    favicon: 'https://github.com/hdsenevi/knowledgebase-career-development/tree/master/src/images/img/hasura_icon_black.svg',
+    docsLocation: 'https://github.com/hdsenevi/knowledgebase-react/tree/master/content',
+    favicon: 'https://github.com/hdsenevi/knowledgebase-react/tree/master/src/images/img/hasura_icon_black.svg',
   },
   pwa: {
     enabled: false, // disabling this will also remove the existing service worker.
